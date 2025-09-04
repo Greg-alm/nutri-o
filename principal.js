91afd074ff8ad8d24bb64f0088bd64e6fc1b35a6
Greg-alm <a.gregory@escola.pr.gov.br>
2025-09-04 12:35:35 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,19 +1,53 @@
-//Alteração do titulo e subtitulo
-var nutri = document.querySelector("h1");
-nutri.textContent = "Paulista Nutrição";
+//MUDA OS TITULOS
+var tsecundario = document.querySelector(".container-titulo-dois");
+tsecundario.textContent = "Meus pacientes";
 
-var pacientes = document.querySelector("h2");
-pacientes.textContent = "Meus clientes";
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var titulo = document.querySelector(".container-titulo");
+titulo.textContent = "João Nutrição";
 
-var tdaltura = paciente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+//SELECIONA PACIENTE
+var pacientes = document.querySelectorAll(".paciente");
 
-var IMC = peso/(altura*altura);
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    
+    //OBTÉM O VALOR PESO
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    console.log(peso);
 
-var tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent =IMC;
+    //OBTÉM O VALOR ALTURA
+    var tdAltura = document.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    console.log(altura);
 
-console.log(paciente);
+    //CALCULA IMC
+    var IMC = peso / (altura * altura);
+
+    //VALORES BOOLEANOS
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(pesoValido && alturaValida){
+        var tdIMC = paciente.querySelector(".info-imc");
+        tdIMC.textContent = IMC;
+    }
+
+    //AVISO DE VALOR INVÁLIDO
+    if(peso <= 0 || peso >= 1000){
+        var pesoValido = false;
+        tdIMC.textContent = "peso invalido";
+        paciente.classList.add("paciente-invalido");
+        
+    }
+
+    if(altura < 0 || altura > 3.00){
+        var alturaValida = false;
+        alert("Altura inválida!");
+        tdIMC.textContent = "altura invalido";
+        paciente.classList.add("pacienter-invalido");
+    }
+
+   
+}
+
